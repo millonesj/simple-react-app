3544ce9bd1878822cc74c7744dcb3255c9e31f22
Jhonny Neciosup Millones <jhonny.millones@gmail.com>
2019-11-20 21:32:05 -0500

    feat(timer.js): Add Timer component, it use state, componentDidMount, componentWillUnmount

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import Greeting from './components/greeting'
+import Timer from './components/timer'
+
 function App() {
   return (
     <div className="App">
       <Greeting message="Jhonny"></Greeting>
+      <Timer></Timer>
     </div>
   );
 }
